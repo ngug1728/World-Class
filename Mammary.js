27a6e485c7fc317bacc3ef5023d3d102e52ebfde
root <root@DESKTOP-THBFQCT.localdomain>
2022-12-01 14:43:04 +0300

    default

--- Mammary.js
+++ Mammary.js
@@ -2,3 +2,6 @@ const root = ReactDOM.createRoot(document.getElemenById('root'));
 export function Library(repo,language) {
   console.log("${repo} ${language}")
 }
+export default function App(){
+return (<Library />)
+}
